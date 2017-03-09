6c3448f176d910530e10c7d445814d5f7a4c4cb8
oznetmaster <ncolvin@nivloc.com>
2017-03-09 17:13:54 +0800

    Update copyright and license

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle ("SSharpCrestronExtensionsLibrary")]
 [assembly: AssemblyCompany ("Nivloc Enterprises Ltd")]
 [assembly: AssemblyProduct ("SSharpCrestronExtensionsLibrary")]
-[assembly: AssemblyCopyright ("Copyright © 2017")]
+[assembly: AssemblyCopyright ("Copyright © 2017 Nivloc Enterprises Ltd")]
 [assembly: AssemblyVersion ("1.0.0.*")]
 
 [assembly: InternalsVisibleTo ("SSMonoIOLibrary")]
