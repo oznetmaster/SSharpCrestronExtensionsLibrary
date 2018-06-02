afdcfcdf137ad2aadfc8a3e92755384e5593ef47
oznetmaster <ncolvin@nivloc.com>
2018-06-03 05:18:44 +0800

    Add IPAddress Extensions

---
+++ IPAddressExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace Crestron.SimplSharp
+	{
+	public static class IPAddressExtensions
+		{
+		public static IPAddress Clone (this IPAddress ipAddress)
+			{
+			return new IPAddress (ipAddress.GetAddressBytes ());
+			}
+		}
+	}
