c7b28fb05e6298015d335a2e5c797f0fb3a457ed
OzNetMaster <ncolvin@nivloc.com>
2018-09-20 16:21:50 -0400

    Add IPAddressEx class with TryParse method

---
+++ IPAddressEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace Crestron.SimplSharp
+	{
+	public static class IPAddressEx
+		{
+		public static bool TryParse (string ipString, out IPAddress address)
+			{
+			try
+				{
+				address = IPAddress.Parse (ipString);
+				return true;
+				}
+			catch (FormatException)
+				{
+				address = IPAddress.None;
+				return false;
+				}
+			}
+		}
+	}
